6406bf84097cc6f74bd1769d513d22a9d57f5caf
Niall Murphy <Niall.Murphy@cl.cam.ac.uk>
2017-06-03 18:30:27 +0100

    Move constants to a new file

---
+++ constants.js
@@ -0,0 +1,14 @@
+
+var frameWidth = 1200.0
+var frameHeight = 800.0
+var playingAreaWidth = 1200.0
+var playingAreaHeight = 760.0
+var frameRate = 30
+var frameInterval = Math.round(1000/frameRate);
+var gameRate = 60
+var gameInterval = Math.round(1000/gameRate);
+var frameCounter = 0;
+var gameCounter = 0;
+var maxFrameCount = 1000000000
+var windowScaling = 1.0
+var tinyFloat = 0.0000001
