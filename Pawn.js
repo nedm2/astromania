62109be965e7d99a0ea38fe764b7f89239baeb49
Niall Murphy <Niall.Murphy@cl.cam.ac.uk>
2017-06-04 00:50:43 +0100

    Move Pawn to new file

---
+++ Pawn.js
@@ -0,0 +1,79 @@
+
+/* Pawn */
+
+var Pawn = function(context, radius, sprites, bulletRadius, bulletSprites,
+      position = new Vector(playingAreaWidth/2, playingAreaHeight/2), velocity = new Vector(0, 0), health=100){
+    Craft.prototype.constructor.call(this, context, position, velocity, health, radius, sprites);
+    this.initialposition = position;
+    this.collisionDamageInflicted = 1;
+    this.mass=600;
+}
+
+Pawn.prototype = new Craft();
+Pawn.prototype.constructor = Pawn;
+
+Pawn.prototype.update = function(){
+    Craft.prototype.update.call(this);
+    if(!this.inPlayingArea()){
+        this.position = this.initialposition
+    }
+}
+
+Pawn.prototype.isEnemyCraft = function(){
+    return true;
+}
+
+Pawn.prototype.collision = function(o){
+    if(o instanceof Bullet){
+        if(o.owner instanceof Ship){
+            this.damage(o.power, 1);
+        }
+    }
+    else if(o instanceof Ship){
+        this.damage(o.collisionDamageInflicted, 1);
+    }
+}
+
+/* ------ Pawn */
+
+/* HomingPawn */
+
+var HomingPawn = function(context, radius, sprites, bulletRadius, bulletSprites,
+      position = new Vector(playingAreaWidth/2, playingAreaHeight/2), velocity = new Vector(0, 0), health=100, target){
+    Pawn.prototype.constructor.call(this, context, radius, sprites, bulletRadius, bulletSprites, position, velocity, health);
+    this.target = target;
+    this.acceleration=0.4;
+    this.resistance = 0.025;
+    this.mass = 100;
+}
+
+HomingPawn.prototype = new Pawn();
+HomingPawn.prototype.constructor = HomingPawn;
+
+HomingPawn.prototype.update = function(){
+    Craft.prototype.update.call(this);
+
+    if(!this.destroyed){
+        /* Find vector from this to target */
+        var dir = this.target.position.sub(this.position);
+
+        /* Snap dir vector to 45 degree increment */
+        var snapdir = directionToVector(vectorToDirection(dir));
+
+        /* Apply a force in the snapped direction */
+        this.forces = snapdir.unit_vector();
+
+        this.updateVelocity();
+    }
+}
+
+HomingPawn.prototype.updateVelocity = function(){
+    this.velocity = this.velocity.add(this.forces.unit_vector().scale(this.acceleration));
+    this.velocity = this.velocity.sub(this.velocity.scale(this.resistance));
+};
+
+HomingPawn.prototype.draw = function(){
+    Craft.prototype.draw.call(this, vectorToDirection(this.forces));
+}
+
+/* ------ Pawn */
