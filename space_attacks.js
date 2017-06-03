af4729a6edb1b664ed79e3bddd8b5c804abf112f
Niall Murphy <Niall.Murphy@cl.cam.ac.uk>
2017-06-04 00:47:49 +0100

    Move Bullet to new file

--- space_attacks.js
+++ space_attacks.js
@@ -88,47 +88,6 @@ Dashboard.prototype.draw = function() {
 };
 
 /* ------------ Dashboard */
-
-/* Bullet */
-
-var Bullet = function(owner, context, position, velocity, radius, sprites, power, enemyfire){
-    Drawable.prototype.constructor.call(this, context, position, radius, sprites);
-    this.owner = owner
-    this.velocity = velocity
-    this.power = power
-    this.enemyfire = enemyfire
-}
-
-Bullet.prototype = new Drawable();
-Bullet.prototype.constructor = Bullet;
-
-Bullet.prototype.update = function(){
-    this.position = this.position.add(this.velocity);
-}
-
-Bullet.prototype.draw = function(){
-    Drawable.prototype.draw.call(this);
-}
-
-Bullet.prototype.getType = function(){
-    return (this.owner.getType() + 'bullet');
-}
-
-Bullet.prototype.isActive = function(){
-    return this.active && this.inPlayingArea();
-}
-
-Bullet.prototype.isFriendlyFire = function(){
-    return !this.enemyfire;
-}
-
-Bullet.prototype.collision = function(o){
-    if(o instanceof Pawn){
-        this.markInactive();
-    }
-}
-
-/* ------ Bullet */
     
 
 /* Pawn */
